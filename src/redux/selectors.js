378ecb0f97768e74ada397de685470e428b5e243
X-RAY <intelekt200012@gmail.com>
2023-11-13 22:14:54 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -4,9 +4,7 @@ export const getCarsIsLoading = state => state.cars.isLoading;
 export const getCarsError = state => state.cars.error;
 export const getCurrentCar = state => state.cars.currentCar;
 export const getShowModal = state => state.cars.showModal;
-
 export const getFilter = state => state.filter;
 export const getClientFilter = state => state.filter.client;
 export const getServerFilter = state => state.filter.server.make;
-
 export const getFavoriteCars = state => state.favorite.favorite;
